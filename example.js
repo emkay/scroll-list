b27a485225d5952513c16f46a65ec8a54a014554
Michael Matuzak <mmatuzak@gmail.com>
2017-07-17 16:57:07 -0700

    chore(example): update example

--- example.js
+++ example.js
@@ -2,18 +2,16 @@ const html = require('bel')
 const ScrollList = require('.')
 const sl = new ScrollList()
 
-const root = html`<ul></ul>`
 const data = []
 
 for (var i = 0; i < 10000; i++) {
   data.push(html`<li>Here is a dang element ${i}</li>`)
 }
 
-let batch = 500
-let index = 0
+const batch = 500
+const index = 0
 
 const el = sl.render({
-  root,
   data,
   batch,
   index
